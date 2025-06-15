953fcd7585e4ff246c4dc5e255027174826d1441
ZicorXXIX <106899726+ZicorXXIX@users.noreply.github.com>
2025-06-15 23:53:15 +0530

    Update options.lua

--- lua/thezicor/options.lua
+++ lua/thezicor/options.lua
@@ -1,5 +1,7 @@
 	vim.opt.backup = false
 	vim.opt.swapfile = false
 	vim.opt.undodir = 'C:'..os.getenv('HOMEPATH') .. '/.nvim/undodir' 
+	-- for WSL
+	-- vim.opt.undodir = os.getenv('HOME') .. '/.nvim/undodir' 
 	vim.opt.undofile = true
 
